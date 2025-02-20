fbcd2f63aafab08fababd4c92e961c0acf2f6ef9
HarunRashid-dev <harunrashid9159@gmail.com>
2025-02-20 07:26:49 +0530

    Code Completed till 8:05

--- app/src/main/java/com/example/learnloginjc/MainActivity.kt
+++ app/src/main/java/com/example/learnloginjc/MainActivity.kt
@@ -4,44 +4,67 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.learnloginjc.ui.theme.LearnLoginJCTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.example.learnloginjc.ui.theme.GreenJC
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            LearnLoginJCTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun LoginScreen(onLoginScreen: ()-> Unit){
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    val context = LocalContext.current.applicationContext
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LearnLoginJCTheme {
-        Greeting("Android")
+    Column (modifier = Modifier.fillMaxSize()
+        .padding(horizontal = 26.dp, vertical = 140.dp),
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally)
+    {
+        OutlinedTextField(value = username, onValueChange = {username = it},
+            label = { Text(text = "Username")},
+            shape = RoundedCornerShape(20.dp),
+            colors = TextFieldDefaults.colors(
+                focusedLeadingIconColor = GreenJC,
+                unfocusedLeadingIconColor = GreenJC,
+                focusedLabelColor = GreenJC,
+                unfocusedLabelColor = GreenJC,
+                focusedContainerColor = Color.White,
+                unfocusedContainerColor = Color.White,
+                focusedIndicatorColor = GreenJC,
+                unfocusedIndicatorColor = GreenJC,
+                unfocusedPlaceholderColor = GreenJC
+            ), leadingIcon = {
+
+            }
+            )
     }
 }
